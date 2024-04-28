1350276ad4cbf26ee07d650e4b470d5299658f78
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-04-28 14:38:51 +0500

    boshidan yaratildi tailwindcss

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,11 +4,11 @@ import Logo from "/Logo.svg";
 function App() {
   return (
     <>
-      <div className="flex w-[78px] m-auto items-center justify-between border-b-[0.3] border-emerald-500 ">
+      <div className=" w-[78px] m-auto flex items-center justify-between border-b-[0.3] border-emerald-500 ">
         <div className="">
-          <img src={Logo} />
+          <img src={Logo} className="relative left-7" />
         </div>
-        <div className="flex gap-[30px]">
+        <div className="flex gap-[30px] m-auto">
           <h3 className="font-sans text-base font-normal text-left relative transition-shadow ">
             Home
           </h3>
